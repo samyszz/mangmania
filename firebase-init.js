ec6aaa00c7c13342022202427af4947929cb31c2
samyszz <samyraalves20007@gmail.com>
2025-06-15 21:38:05 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,19 +1,17 @@
-// firebase-init.js
+/ firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 
-// Configuração do Firebase lendo as Variáveis de Ambiente da Vercel
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyDJVOrQOqvGkuaMLlPUWzaFndA0kn120cI",
+  authDomain: "mangamania-essevai.firebaseapp.com",
+  projectId: "mangamania-essevai",
+  storageBucket: "mangamania-essevai.firebasestorage.app",
+  messagingSenderId: "39272425315",
+  appId: "1:39272425315:web:fb48d79a44ad5613059556",
+  measurementId: "G-P7QGNHZPS8",
 };
 
-// Inicializa o Firebase e exporta a autenticação
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export { auth };
